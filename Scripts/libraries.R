8485d66ae8a9b02f36426da94f73ef87de12731d
Richard Winter <r.winter@uni-mannheim.de>
2022-07-28 15:26:55 +0200

    Initial commit. Setup for github repo.

---
+++ Scripts/libraries.R
@@ -0,0 +1,11 @@
+#.libPaths(paste0(root,"Packages/")) # Package library
+
+if (!require(pacman)){
+  install.packages("pacman",repos = "http://cran.us.r-project.org")
+}
+
+pacman::p_load(
+  data.table,
+  tidyverse,
+  modelsummary
+)
